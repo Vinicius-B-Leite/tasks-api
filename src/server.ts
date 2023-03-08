b9bb4768d9e42a41603b7a4cd0a866a891fdb872
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-03-08 10:42:35 -0300

    structure: adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ dotenv.config()
 import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
 import routes from './routes'
-
+import cors from 'cors'
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
